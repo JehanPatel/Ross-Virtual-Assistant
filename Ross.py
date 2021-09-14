666fbece257ce71f271d5a0d728e8aa051e23786
Jehan Patel <90050088+JehanPatel@users.noreply.github.com>
2021-09-14 21:45:33 +0530

    Add files via upload

---
+++ Ross.py
@@ -0,0 +1,103 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine. getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("Hi i am Ross. How may I help you ?")
+
+
+def takeCommand():
+    
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening..")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+            
+        print("I am sorry i couldn't get that...")  
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    
+        query = takeCommand().lower()
+
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open gmail' in query:
+            webbrowser.open("accounts.google.com")
+
+        elif 'open discord' in query:
+            webbrowser.open("discord.com")
+
+        elif 'open shit ' in query:
+            webbrowser.open("artisticshit.home.blog")    
+
+
+        elif 'play music' in query:
+            music_dir = "C:\\Users\\91923\\Downloads\\Songs" #Change the directory according to your song file location
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'what is the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            #Enter the code directory of this file according to your system
+
+            codePath = "C:\\Users\\91923\\AppData\\Local\\Programs\\Python\\Python39\\Ross.py"
+            os.startfile(codePath)
+
+
+
+
+
